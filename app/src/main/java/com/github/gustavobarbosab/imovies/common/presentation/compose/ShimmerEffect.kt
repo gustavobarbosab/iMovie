ecb2101807922a3ca68d96b4385017be969d14f5
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2024-10-28 13:49:08 -0300

    creating shimmer

---
+++ app/src/main/java/com/github/gustavobarbosab/imovies/common/presentation/compose/ShimmerEffect.kt
@@ -0,0 +1,47 @@
+package com.github.gustavobarbosab.imovies.common.presentation.compose
+
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.IntSize
+import com.github.gustavobarbosab.imovies.presentation.theme.Grey10
+import com.github.gustavobarbosab.imovies.presentation.theme.Grey20
+import com.github.gustavobarbosab.imovies.presentation.theme.Grey50
+import com.github.gustavobarbosab.imovies.presentation.theme.Grey95
+
+fun Modifier.shimmerEffect(): Modifier = composed {
+    var size by remember {
+        mutableStateOf(IntSize.Zero)
+    }
+    val transition = rememberInfiniteTransition()
+    val startOffsetX by transition.animateFloat(
+        initialValue = -2 * size.width.toFloat(),
+        targetValue = 2 * size.width.toFloat(),
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000)
+        ),
+        label = "shimmer animation"
+    )
+
+    background(
+        brush = Brush.linearGradient(
+            colors = listOf(Grey50, Grey20, Grey50),
+            start = Offset(startOffsetX, 0f),
+            end = Offset(startOffsetX + size.width.toFloat(), size.height.toFloat())
+        )
+    ).onGloballyPositioned {
+        size = it.size
+    }
+}
