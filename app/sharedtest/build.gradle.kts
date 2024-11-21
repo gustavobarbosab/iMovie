eed2223e0e1a7af87d96854f6f7a28b195b4d9cb
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2024-11-21 17:50:52 -0300

    creating shared test module

---
+++ app/sharedtest/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "com.github.gustavobarbosab.imovies.sharedtest"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+    sourceSets {
+        getByName("main") {
+            kotlin.srcDir("src/main/kotlin")
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":app"))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.junit)
+    implementation(libs.ext.junit)
+    implementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.compose.test.junit4)
+    implementation(libs.hilt.test)
+    implementation(libs.hilt.compose)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+}
